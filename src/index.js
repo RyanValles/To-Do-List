927bfecd00496d64255ad20cedf2b431a948ad1a
Ryan <ravalles10@gmail.com>
2023-01-27 14:30:38 +0900

    Add generate function to add task button

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import {generate} from './add-task.js';
+
+export const tasks = document.querySelector('#tasks')
+export const container = document.querySelector('#container')
+const button = document.querySelector('#add');
+button.addEventListener('click', generate)
+
